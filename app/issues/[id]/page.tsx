b01571cda7df1d054ea8fc7aab93aec9fa8a118b
Sahal Imran <sahalimran.pro@gmail.com>
2024-05-08 00:46:21 +0530

    Add Edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,32 +1,42 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge'
-import prisma from '@/prisma/client'
-import { Card, Flex, Heading, Text } from '@radix-ui/themes'
-import { notFound } from 'next/navigation'
-import ReactMarkdown from 'react-markdown'
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import prisma from "@/prisma/client";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
+import ReactMarkdown from "react-markdown";
+import {Pencil2Icon} from "@radix-ui/react-icons"
+import Link from "next/link";
 
-interface Props{
-    params:{id:string}
+interface Props {
+  params: { id: string };
 }
-const IssueDetailsPage = async({params:{id}}:Props) => {
-    if(typeof parseInt(id)!=='number') notFound();
-    const issue = await prisma.issue.findUnique({
-        where:{id:parseInt(id)}
-    })
-    if(!issue){
-        notFound()
-    }
+const IssueDetailsPage = async ({ params: { id } }: Props) => {
+  if (typeof parseInt(id) !== "number") notFound();
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(id) },
+  });
+  if (!issue) {
+    notFound();
+  }
   return (
-    <div>
+    <Grid columns={{initial:"1",md:"2"}} gap="5">
+      <Box>
         <Heading>{issue.title}</Heading>
-        <Flex gap='3' my="2">
-        <IssueStatusBadge status={issue.status}/>
-        <Text>{issue.createdAt.toDateString()}</Text>
+        <Flex gap="3" my="2">
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
         </Flex>
-        <Card className='prose' mt='4'>
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
+        <Card className="prose" mt="4">
+          <ReactMarkdown>{issue.description}</ReactMarkdown>
         </Card>
-    </div>
-  )
-}
+      </Box>
+      <Box>
+        <Button>
+            <Pencil2Icon/>
+            <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
+  );
+};
 
-export default IssueDetailsPage
+export default IssueDetailsPage;
