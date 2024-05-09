8eae6ca446dc9c74fbe1e04cf4a9b930603e9780
Sahal Imran <sahalimran.pro@gmail.com>
2024-05-10 02:32:09 +0530

    Build an api for deleteing issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -2,32 +2,54 @@ import { issueSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-interface Props{
-    params:{params:{id:string}}
+interface Props {
+  params: { params: { id: string } };
 }
 
 export async function PATCH(
-    request: NextRequest,
-    {params}:{params:{id:string}}){
-    const body = await request.json()
-    const validation = issueSchema.safeParse(body)
-    if(!validation.success){
-        return NextResponse.json(validation.error.format(),{status:400})
-    }
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+  if (!issue) {
+    return NextResponse.json({ message: "Issue not found" }, { status: 404 });
+  }
+  const updatedIssue = await prisma.issue.update({
+    where: { id: issue.id },
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+  return NextResponse.json(updatedIssue, { status: 200 });
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
     const issue = await prisma.issue.findUnique({
         where:{
             id:parseInt(params.id)
         }
-    });
+    })
     if(!issue){
         return NextResponse.json({message:'Issue not found'},{status:404})
     }
-    const updatedIssue =  await prisma.issue.update({
-        where:{id:issue.id},
-        data:{
-            title:body.title,
-            description:body.description
+
+    await prisma.issue.delete({
+        where:{
+            id:parseInt(params.id)
         }
     })
-    return NextResponse.json(updatedIssue,{status:200})
+    return NextResponse.json({message:'Issue deleted successfully'},{status:200})
 }
