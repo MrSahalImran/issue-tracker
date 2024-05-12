a80c0a5e2b2cc2590725100bfa5a128898f3d76b
Sahal Imran <sahalimran.pro@gmail.com>
2024-05-12 20:21:49 +0530

    Secure the application

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,11 +1,17 @@
 import { issueSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
+import authOptions from "../../auth/authOptions";
 
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
   if (!validation.success) {
@@ -33,19 +39,26 @@ export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-    const issue = await prisma.issue.findUnique({
-        where:{
-            id:parseInt(params.id)
-        }
-    })
-    if(!issue){
-        return NextResponse.json({message:'Issue not found'},{status:404})
-    }
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+  if (!issue) {
+    return NextResponse.json({ message: "Issue not found" }, { status: 404 });
+  }
 
-    await prisma.issue.delete({
-        where:{
-            id:parseInt(params.id)
-        }
-    })
-    return NextResponse.json({message:'Issue deleted successfully'},{status:200})
+  await prisma.issue.delete({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+  return NextResponse.json(
+    { message: "Issue deleted successfully" },
+    { status: 200 }
+  );
 }
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,13 +3,16 @@ import { Box, Flex, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
-import DeleteIssueButton from "./deleteIssueButton";
+import DeleteIssueButton from "./DeleteIssueButton";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/api/auth/authOptions";
 
 interface Props {
   params: { id: string };
 }
 const IssueDetailsPage = async ({ params: { id } }: Props) => {
   if (typeof parseInt(id) !== "number") notFound();
+  const session = await getServerSession(authOptions);
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(id) },
   });
@@ -17,16 +20,18 @@ const IssueDetailsPage = async ({ params: { id } }: Props) => {
     notFound();
   }
   return (
-    <Grid columns={{initial:"1",sm:"5"}} gap="5">
+    <Grid columns={{ initial: "1", sm: "5" }} gap="5">
       <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
-      <Box>
-        <Flex direction='column' gap='4'>
-        <EditIssueButton issueId={issue.id}/>
-        <DeleteIssueButton issueId={issue.id}/>
-        </Flex>
-      </Box>
+      {session && (
+        <Box>
+          <Flex direction="column" gap="4">
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
   );
 };
